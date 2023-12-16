3c1bbabb169e4cfaa87ea634c830622079a471d7
Sabina Shvetsova <sabinabogomazova@gmail.com>
2023-12-16 13:35:41 +0300

    add todo elements crud

--- src/App.js
+++ src/App.js
@@ -4,20 +4,62 @@ import { useState } from "react";
 const INITIAL_TASKS = ["Feed a dog", "Wash the car", "Paint the walls"];
 
 function App() {
-  const [tasks, setTasks] = useState(INITIAL_TASKS);
+  const [allTasks, setAllTasks] = useState(INITIAL_TASKS);
+  const [newTask, setNewTask] = useState("");
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [isShownAllTasks, setIsShownAllTasks] = useState(true);
+  const [tasksToShow, setTasksToShow] = useState(allTasks);
+
+  function addNewTask() {
+    setAllTasks([...allTasks, newTask]);
+    setNewTask("");
+    setTasksToShow(isShownAllTasks ? [...allTasks, newTask] : completedTasks);
+  }
+
+  function removeTask(taskToRemove) {
+    setAllTasks(allTasks.filter((task) => task !== taskToRemove));
+    setCompletedTasks(completedTasks.filter((task) => task !== taskToRemove));
+
+    setTasksToShow(
+      isShownAllTasks
+        ? allTasks.filter((task) => task !== taskToRemove)
+        : completedTasks.filter((task) => task !== taskToRemove)
+    );
+  }
+
+  function completeTask(task) {
+    setCompletedTasks([...completedTasks, task]);
+  }
+
+  function showCompletedTasks() {
+    setTasksToShow(() => (isShownAllTasks ? completedTasks : allTasks));
+    setIsShownAllTasks((isShown) => !isShown);
+  }
 
   return (
     <>
       <h1>TODO list</h1>
       <ul>
-        {tasks.map((task) => (
+        {tasksToShow.map((task) => (
           <li key={task}>
-            <input type="checkbox" />
+            <input type="checkbox" onClick={() => completeTask(task)} />
             {task}
-            <button type="button">x</button>
+            <button type="button" onClick={() => removeTask(task)}>
+              x
+            </button>
           </li>
         ))}
       </ul>
+      <div>
+        <input
+          value={newTask}
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+        <button onClick={addNewTask}>Add new task</button>
+      </div>
+      <button onClick={showCompletedTasks}>
+        {isShownAllTasks ? "Show completed tasks" : "Show all tasks"}
+      </button>
     </>
   );
 }
